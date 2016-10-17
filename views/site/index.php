51a5744c5a2f6fcf6525081df34e0baa8434b03f
lcmwango <chizimwango@gmail.com>
2016-10-17 11:51:24 +0300

    test

--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,7 @@ $this->title = 'BP--Shinda 2';
     <div class="jumbotron">
         <h1>Shinda 2 BP Monitoring Tool</h1>
 
-        <p><?= Html::a('Enter', ['/bp24h/index'],['class'=>'btn btn-success']); ?></p>
+        <p><?= Html::a('Enter ', ['/bp24h/index'],['class'=>'btn btn-success']); ?></p>
         <p><?= Html::a('Enter', ['/participant/index'],['class'=>'btn btn-success']); ?></p>
     </div>
 
